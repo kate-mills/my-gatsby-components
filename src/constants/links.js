7e7547423cc4089c497292054431b07bc5f3160d
Kate Mills <kate.mills1124@gmail.com>
2020-07-09 08:18:22 -0700

    navigation constants

---
+++ src/constants/links.js
@@ -0,0 +1,73 @@
+export default [
+  {
+    path: '/',
+    text: 'home',
+    menu: [],
+  },
+  {
+    path: '/about',
+    text: 'about',
+    menu: [],
+  },
+  {
+    path: '/products',
+    text: 'products',
+    menu: [],
+  },
+  {
+    path: '/why-mc',
+    text: 'why mc?',
+    menu: [
+      {
+        path: 'before-and-after',
+        text: 'before and after',
+      },
+      {
+        path: 'rave-reviews',
+        text: 'rave reviews',
+      },
+      {
+        path: 'press',
+        text: 'press',
+      },
+      {
+        path: 'images-and-logos',
+        text: 'Product Images & Logos',
+      },
+    ],
+  },
+  {
+    path: '/contact',
+    text: 'contact',
+    menu: [],
+  },
+  {
+    path: 'pro-orders',
+    text: 'pro orders',
+    menu: [],
+  },
+  {
+    path: '/professionals',
+    text: 'professionals',
+    menu: [],
+  },
+  {
+    path: '/education',
+    text: 'education',
+    menu: [
+      {
+        path: 'upcoming-classes',
+        text: 'classes',
+      },
+      {
+        path: 'treatment-protocol-videos',
+        text: 'treatment protocol & videos',
+      },
+      {
+        path: 'articles',
+        text: 'articles',
+      },
+    ],
+  },
+]
+
---
+++ src/constants/social.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { GrFacebook, GrYoutube, GrInstagram } from 'react-icons/gr'
+
+export default [
+  {
+    icon: <GrFacebook />,
+    url: 'https://www.facebook.com/michelecorleyclinicalskincare/',
+    class: 'facebook',
+  },
+  {
+    icon: <GrYoutube />,
+    url: 'https://www.youtube.com/channel/UCjVjZL0axnqV9EFCrCp3yaQ',
+    class: 'youtube',
+  },
+  {
+    icon: <GrInstagram />,
+    url: 'https://www.instagram.com/michelecorleyskincare/',
+    class: 'instagram',
+  },
+]
+
