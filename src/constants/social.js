a945d464d22ffb5b4d6758232400bc4491a3566f
Kate Mills <kate.mills1124@gmail.com>
2020-07-13 15:27:23 -0700

    Moved Youtube link to Vimeo

--- src/constants/social.js
+++ src/constants/social.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { GrFacebook, GrYoutube, GrInstagram } from 'react-icons/gr'
+import React from "react"
+import { GrFacebook, GrVimeo, GrInstagram } from "react-icons/gr"
 
 export default [
   {
     icon: <GrFacebook />,
-    url: 'https://www.facebook.com/michelecorleyclinicalskincare/',
-    class: 'facebook',
+    url: "https://www.facebook.com/michelecorleyclinicalskincare/",
+    class: "facebook",
+    style: { color: "#3b998", background: "white" },
   },
   {
-    icon: <GrYoutube />,
-    url: 'https://www.youtube.com/channel/UCjVjZL0axnqV9EFCrCp3yaQ',
-    class: 'youtube',
+    icon: <GrVimeo />,
+    url: "https://vimeo.com/michelecorley",
+    class: "vimeo",
+    style: { color: "#00adef", background: "white" },
   },
   {
     icon: <GrInstagram />,
-    url: 'https://www.instagram.com/michelecorleyskincare/',
-    class: 'instagram',
+    url: "https://www.instagram.com/michelecorleyskincare/",
+    class: "instagram",
+    style: { color: "#fff" },
   },
 ]
-
