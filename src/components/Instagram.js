648176c026672ae9865ef40a32b37d3773cc2b8c
Kate Mills <kate.mills1124@gmail.com>
2020-07-14 15:14:55 -0700

    Much clearer instagram icon image

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -1,5 +1,5 @@
 import React from "react"
-import icon from "../images/insta-icon.jpg"
+import icon from "../images/instagram-icon.jpg"
 
 const Instagram = () => {
   return <img src={icon} width="58" height="58" alt="instagram icon" />
