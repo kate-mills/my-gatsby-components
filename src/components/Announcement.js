bedc2f5bafa5c8776c052806900166d3c29209ee
Kate Mills <kate.mills1124@gmail.com>
2020-07-17 19:39:31 -0700

    Announcemnt will have to do for now

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -5,32 +5,33 @@ import styled from 'styled-components'
 const Announcement = ({className}) => {
   return (
     <div className={className}>
-      <p className="p"></p>
-      <p className="p">707.637.4996 ~ info@michelecorley.com</p>
-      <p className="p">x</p>
-      <p className="p"></p>
+      <p className="p-1">707.637.4996 ~ info@michelecorley.com</p>
+      <p className="p-2"><span>x</span></p>
     </div>
   )
 }
 
 const WrappedAnnouncement = styled(Announcement)`
+
 & {
-  display: flex;
-  flex-flow: row nowrap;
-  background: var(--mccPink);
-  width:100vw;
-  justify-content: stretch;
-}
-& p {
-  font-family: "Times New Roman", Times, serif;
+  font-family: helvetica,arial,sans-serif;
   font-size: 18px;
   line-height: 22px;
   color: #ffffff;
   font-style: normal;
   font-weight: 400;
+  width: 100vw;
+  margin: 0;
+  padding-bottom: 20px;
   background: var(--mccPink);
-  margin: 0 auto;text-align: center;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  height: 60ppx;
 }
-
+& p {text-align: center;margin: 0; }
+& p.p-1 {width: 80%;}
+& p.p-2 {width: 50px;  display: flex; align-items: center; justify-content: center;
+    background: #d9564f; text-align: center; height: 50px;}
 `
 export default WrappedAnnouncement
