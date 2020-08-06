be9300975e9dc516408132371d709896a6b0cbdc
Kate Mills <kate.mills1124@gmail.com>
2020-08-06 12:16:06 -0700

    form contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,7 @@ const ContactPage = ({data}) => {
     <PageModel title="Contact">
       <PageWrapper>
         <div className="flex-col">
-          <div className="flex-item">
+          <div className="flex-item-1">
             <div className="contact-text-container">
               <h2
                 style={{
@@ -23,10 +23,10 @@ const ContactPage = ({data}) => {
                 }}>
                 Have questions about Michele Corley Clinical Skin Care?
               </h2>
-              <p>
+              <p className="contact-p">
                 Please contact us via the submission form or give us a call at 707.637.4996 and we'll get back to you as soon as we can. If you are a licensed professional, please include your license number. 
               </p>
-              <p>
+              <p className="contact-p">
                 Thank you!
               </p>
             </div>
@@ -34,7 +34,7 @@ const ContactPage = ({data}) => {
               <Image fluid={data.file.childImageSharp.fluid}/>
             </div>
           </div>
-          <div className="flex-item"><ContactForm/></div>
+          <div className="flex-item-2"><ContactForm/></div>
         </div>
       </PageWrapper>
     </PageModel>
@@ -42,56 +42,49 @@ const ContactPage = ({data}) => {
 }
 
 const PageWrapper = styled.section`
-  &{
-    padding: 4rem 0;
-  } 
   & .flex-col {
+    align-items: flex-start;
     display:flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: space-around;
-    width: 90%;
     margin: 0 auto;
   }
-  & .flex-col .flex-item {
-    width: 40%;
-  }
-  & .flex-col .flex-item .contact-img-container {
+  & .flex-col .flex-item-1 .contact-p {
     width: 80%;
-    border: 10px solid var(--mainWhite);
-    border-radius: 25px;
-    margin-inline-start: 10%;
-    margin-inline-end: 10%;
   }
-  & .flex-col .flex-item .contact-text-container {
-    text-align: left;
-    padding-inline-start: 10%;
-    padding-inline-end: 10%;
-    white-space: pre-line;
+  & .contact-img-container{
+    max-width: 500px;
   }
-  & .flex-col .flex-item .contact-text-container p {
-   font-size: 15px;
-   line-height: 27px;
-   color: #5c5c5c;
-   font-weight: 300;
-   margin: 30px auto;
-  }
+  & .flex-col .flex-item-1  { margin: 0 auto; width: 50%; }
+  & .flex-col .flex-item-2  { margin: 0 auto; width: 40%; }
 
   @media (max-width: 767px) {
-    & .flex-col .flex-item{
-      width: 80%;
+    & .flex-col .flex-item-1{ 
+      width: 100%; 
+    }
+    & .flex-col .flex-item-1 h2{
+      text-align: center;
+    }
+    & .flex-col .flex-item-1 .contact-p {
+      margin: 0 auto;
+      width: 90%;
+    }
+    & .flex-col .flex-item-1 .contact-img-container{
+      margin: 0 auto;
+    }
+    & .flex-col .flex-item-2{ 
+      margin: 0 auto;
+      width: 70%; 
     }
   }
-
-  
-  
 `
 
 export const query = graphql`
   {
     file(relativePath: {eq: "contact-img.png"}) {
       childImageSharp {
-        fluid {
+        fluid(maxWidth: 500) {
           ...GatsbyImageSharpFluid
         }
       }
