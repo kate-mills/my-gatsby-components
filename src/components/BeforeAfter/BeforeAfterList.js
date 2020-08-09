3114d70a52f36a0936c1bf819937ce7481a0c2b9
Kate Mills <kate.mills1124@gmail.com>
2020-08-09 15:05:02 -0700

    updated css class name

--- src/components/BeforeAfter/BeforeAfterList.js
+++ src/components/BeforeAfter/BeforeAfterList.js
@@ -3,7 +3,7 @@ import BeforeAfter  from "./BeforeAfter"
 
 const BeforeAfterList  = ({ bal  }) => {
   return (
-    <section className="article-list">
+    <section className="ba-list">
       {bal.map((data)  => {
         return <BeforeAfter  key={data.id} data={data}/>
       })}
