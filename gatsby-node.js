7d0574bc8c8c875f2e73c54cdc7b05ca2327a616
Kate Mills <kate.mills1124@gmail.com>
2020-08-11 15:42:36 -0700

    Nested createPage fn

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,41 +1,6 @@
 const path = require(`path`)
 const axios = require(`axios`)
 
-exports.createPages = ({ actions }) => {
-  const { createPage } = actions
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`https://kate-mills.github.io/mcc-product-list/products.json`)
-      .then(result => {
-        const { data } = result
-
-        createPage({
-          path: `/search`,
-          component: path.resolve(`./src/templates/ClientSearchTemplate.js`),
-          context: {
-            productData: {
-              allProducts: data.products,
-              options: {
-                indexStrategy: `Prefix match`,
-                searchSanitizer: `Lower Case`,
-                NameIndex: true,
-                CategoryIndex: true,
-                SearchByTerm: true,
-              },
-            },
-          },
-        })
-        resolve()
-      })
-      .catch(err => {
-        console.log(`====================================`)
-        console.log(`error creating Page:${err}`)
-        console.log(`====================================`)
-        reject(new Error(`error on page creation:\n${err}`))
-      })
-  })
-}
-
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
 
@@ -70,4 +35,37 @@ exports.createPages = async({ graphql, actions  }) => {
       context: {slug: product.slug}
     })
   })
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`https://kate-mills.github.io/mcc-product-list/products.json`)
+      .then(result => {
+        const { data } = result
+
+        createPage({
+          path: `/search`,
+          component: path.resolve(`./src/templates/ClientSearchTemplate.js`),
+          context: {
+            productData: {
+              allProducts: data.products,
+              options: {
+                indexStrategy: `Prefix match`,
+                searchSanitizer: `Lower Case`,
+                NameIndex: true,
+                CategoryIndex: true,
+                SearchByTerm: true,
+              },
+            },
+          },
+        })
+        resolve()
+      })
+      .catch(err => {
+        console.log(`====================================`)
+        console.log(`error creating Page:${err}`)
+        console.log(`====================================`)
+        reject(new Error(`error on page creation:\n${err}`))
+      })
+  })
+
 }
