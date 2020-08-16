35a07adc1e4f780ad04d00c8023555e8512a79ac
Kate Mills <kate.mills1124@gmail.com>
2020-08-16 10:28:10 -0700

    slimmed down gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,4 @@
 const path = require(`path`)
-const axios = require(`axios`)
-
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
-
-  // page.matchPath is a special key that's used for matching pages
-  // only on the client.
-  if (page.path.match(/^\/app/)) {
-    page.matchPath = `/app/*`
-
-    // Update the page.
-    createPage(page)
-  }
-}
-
-
 
 exports.createPages = async({ graphql, actions  }) => {
   const {createPage} = actions
@@ -35,37 +19,18 @@ exports.createPages = async({ graphql, actions  }) => {
       context: {slug: product.slug}
     })
   })
-
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`https://kate-mills.github.io/mcc-product-list/products.json`)
-      .then(result => {
-        const { data } = result
-
-        createPage({
-          path: `/search`,
-          component: path.resolve(`./src/templates/ClientSearchTemplate.js`),
-          context: {
-            productData: {
-              allProducts: data.products,
-              options: {
-                indexStrategy: `Prefix match`,
-                searchSanitizer: `Lower Case`,
-                NameIndex: true,
-                CategoryIndex: true,
-                SearchByTerm: true,
-              },
-            },
-          },
-        })
-        resolve()
-      })
-      .catch(err => {
-        console.log(`====================================`)
-        console.log(`error creating Page:${err}`)
-        console.log(`====================================`)
-        reject(new Error(`error on page creation:\n${err}`))
-      })
-  })
-
 }
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/app/)) {
+    page.matchPath = `/app/*`
+
+    // Update the page.
+    createPage(page)
+  }
+}
+
