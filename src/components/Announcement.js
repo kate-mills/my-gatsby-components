2bbbabf7df7522cf0f9e6d95b9d874a38d3cf4b0
Kate Mills <kate.mills1124@gmail.com>
2020-08-16 11:15:45 -0700

    class Announcement

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -4,29 +4,33 @@ import {screen} from "../css/js/media-functions"
 import {handleStatus, hideAnnouncement} from "../utils/announcement"
 
 
-const Announcement = ({ className }) => {
-  let details = <div/>
-  console.log('hideAnouncement', hideAnnouncement())
-
-  if (!hideAnnouncement()){
-    details = (
-      <div id="x" className={`${className} grid-container`}>
-        <div className="grid-item"></div>
-        <div className="grid-item">
-          <p style={{color: "var(--mainWhite)"}}> 707.637.4996 ~ info@michelecorley.com</p>
-        </div>
-        <div className="grid-item">
-          <button
-            onClick={ () => {
-              handleStatus()
-              document.getElementById("x").style.display = "none"
-            }}
-          >x</button>
-        </div>
-      </div>
-      )
+class Announcement extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      details: false,
+      showDetails:!hideAnnouncement() 
+    }
   }
-  return details
+  componentDidMount(){
+    console.log('hideAnouncement', hideAnnouncement(), this.state.showDetails)
+    if (this.state.showDetails===true){
+      this.setState({
+        details: (<div id="x" className={`${this.props.className} grid-container`}>
+          <div className="grid-item"></div>
+          <div className="grid-item">
+            <p style={{color: "var(--mainWhite)"}}> 707.637.4996 ~ info@michelecorley.com</p>
+          </div>
+          <div className="grid-item">
+            <button onClick={ () => { handleStatus(); document.getElementById("x").style.display = "none"; }}>x</button>
+          </div>
+        </div>)
+      }, console.log(this.state))
+    }
+  }
+  render(){
+    return this.state.details
+  }
 }
 
 const WrappedAnnouncement = styled(Announcement)`
