f3a9775cb9c00c1aba43ccb7479d0f48acdaf31d
Kate Mills <kate.mills1124@gmail.com>
2020-08-16 13:07:13 -0700

    Class announcement

--- src/utils/announcement.js
+++ src/utils/announcement.js
@@ -1,11 +1,12 @@
 const isBrowser = typeof window !== `undefined`
 
 const setStatus = noAnnouncement => (window.localStorage.closeAnnouncement = JSON.stringify(1))
-export const checkStatus = () => window.localStorage.closeAnnouncement ? JSON.parse(window.localStorage.closeAnnouncement) : {}
+
+export const checkStatus = () => window.localStorage.closeAnnouncement ? JSON.parse(window.localStorage.closeAnnouncement) : 0
 
 export const handleStatus = () => {
   if (!isBrowser) return false
   setStatus();
 }
 
-export const hideAnnouncement = () => isBrowser && checkStatus()===1
+export const checkShowStatus = () => isBrowser && checkStatus()===1
