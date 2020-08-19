8f2022eebd4792f655f1d28806569ee04dc52ec3
Kate Mills <kate.mills1124@gmail.com>
2020-08-19 16:21:25 -0700

    styled Email

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import styled from "styled-components"
 
-const Email = ({subject, color})=> {
-  let fmtSubject = subject.split(' ').join("%20")
-  let cssColor = color || "inherit"
+const Email = ({subject, color, fontWeight, className})=> {
+  let fmtSubject = subject.split(' ').join("%20") || ""
   return (
-    <a
-      style={{textDecoration:"none", color: cssColor}}
-    href={`mailto:customerservice@michelecorley.com?subject=${fmtSubject}`}><span className="email-prefix">customerservice</span>@michelecorley.com</a>
+    <a className={className}
+      href={`mailto:customerservice@michelecorley.com?subject=${fmtSubject}`}>
+      <span className="email-prefix">customerservice</span>
+      <span className="email-suffix">@michelecorley.com</span>
+    </a>
   )
 }
-export default Email
+export default styled(Email)`
+& {
+ color: ${props => props.color ? props.color : "inherit"};
+ font-weight: ${props => props.fontWeight || "inherit"};
+ text-decoration: none;
+}
+@media (max-width: 360px){
+  & {
+    display: block;
+    font-size: 17px;
+    white-space: pre-wrap;
+  }
+}
+@media(max-width: 295px){
+  & > span {
+    display: block;
+  }
+}
+`
