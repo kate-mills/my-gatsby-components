b25060794dd4070c99ffb9115606f7bf024e8e2d
Kate Mills <kate.mills1124@gmail.com>
2020-08-21 14:01:58 -0700

    Vimeo video plays inline

--- src/components/Products/ProductPlayer.js
+++ src/components/Products/ProductPlayer.js
@@ -1,38 +1,35 @@
 import React from "react"
 import ReactPlayer from "react-player/vimeo"
-// https://github.com/CookPete/react-player#config-prop
-// https://github.com/CookPete/react-player
 
 class VimeoPlayer extends React.Component {
   render() {
     return (
-    this.props.vimeoUrl ? (
-    false ) : (
+
       <ReactPlayer
-        className="react-player aspect-ratio-box"
         url={this.props.url}
-        width="427px"
-        height="240px"
-        controls
+        className={`react-player aspect-ratio-box`}
+        width="425px"
+        height="238px"
+        controls={true}
+        playsinline={true}
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
         config={{
           vimeo: {
-            controls: true,
             playerOptions: {
-              color: "eef4f7",
-              title: true,
-              width: "427px",
-              height: "240px",
-              portrait: true,
-              playing: false,
-              playsinline: true,
+              autopause: true,
+              byline: true,
               controls: true,
-              frameborder: "0",
-              allow: "autoplay; fullscreen",
+              portrait: true,
+              title: true,
+              playing: false,
+              frameborder: false,
             },
           },
         }}
       />
-    )
-  )}
+    )}
 }
 export default VimeoPlayer
