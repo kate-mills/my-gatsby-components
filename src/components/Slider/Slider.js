61c9680f1a4a08327ff680f71886d29c5f1ce053
Kate Mills <kate.mills1124@gmail.com>
2020-08-23 09:59:52 -0700

    Border-radius and refactor

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -7,17 +7,14 @@ export default function Slideshow({images=[], interval=3000}){
 
     useEffect(()=>{
 
+      console.log('use Effect', currentSlide);
       const loop = setInterval(()=>{ 
-          if(currentSlide === images.length-1){ 
-            setCurrentSlide(0); 
-          }else{ 
-            setCurrentSlide(currentSlide+1); 
-          } 
+        currentSlide === images.length-1?
+          setCurrentSlide(c=>0):
+          setCurrentSlide(c=>c+1)
         }, interval); return () => clearInterval(loop);}, [images, currentSlide, interval]);
 
     return (
-        <div data-slideshow>
           <Img fluid={images[currentSlide].childImageSharp.fluid} alt="" className={styles.fade__in__out}/>
-        </div>
     )
 }
