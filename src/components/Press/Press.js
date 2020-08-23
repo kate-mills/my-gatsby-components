a561279cad995ede91ad227f6baa66c8bc163e96
Kate Mills <kate.mills1124@gmail.com>
2020-08-23 13:54:05 -0700

    move items up

--- src/components/Press/Press.js
+++ src/components/Press/Press.js
@@ -4,15 +4,13 @@ import styles from "./press.module.css"
 
 const Press = ({data:{data}}) => {
   return(
-    <div className={styles.pr__row}
-      style={{marginTop: "90px"}}
-    >
+    <div className={styles.pr__row}>
         <Img fluid={data.image.localFiles[0].childImageSharp.fluid}
         className={styles.pr__col}
         />
       <div className={`${styles.pr__col} ${styles.pr__flexible__col}`}>
         <p>{data.summary}</p>
-        {data.link ?  
+        {data.link ?
           <a 
             href={data.link}
             className={`${styles.read__more} btn btn-white`}
@@ -21,7 +19,6 @@ const Press = ({data:{data}}) => {
             >Read More</a>:false
         }
       </div>
-
     </div>
   )
 }
