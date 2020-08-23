d59f92f216962778f339c8f42da5e280f51734bd
Kate Mills <kate.mills1124@gmail.com>
2020-08-23 09:17:46 -0700

    PrivateRoute w/comments

--- src/components/Professionals/PrivateRoute.js
+++ src/components/Professionals/PrivateRoute.js
@@ -1,16 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
-
 import { navigate } from "gatsby"
-
 import { isLoggedIn } from "../../utils/auth"
 
-const PrivateRoute = ({ component: Component, location, title, ...rest }) => {
+const PrivateRoute = ({ component: Component, location, ...rest }) => {
   if (!isLoggedIn() && location.pathname !== `/app/login`) {
+    // If we’re not logged in, redirect to the home page.
     navigate(`/app/login`)
     return null
   }
-  return( <Component {...rest} />) 
+
+  return <Component {...rest} />
 }
 
 PrivateRoute.propTypes = {
