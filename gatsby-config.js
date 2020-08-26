bf2dad9cd10bd9fc349842596e9e5ecbb9d95f90
Kate Mills <kate.mills1124@gmail.com>
2020-08-25 19:55:16 -0700

    preload-font-cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,19 @@ module.exports = {
         theme_color: `#ff665e`,
         display: `standalone`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+            purpose: 'maskable',
+          },
+        ],
         crossOrigin: `use-credentials`,
       },
     },
@@ -124,5 +137,6 @@ module.exports = {
         ]
       }
     },
+    `gatsby-plugin-preload-fonts`,
   ]
 }
