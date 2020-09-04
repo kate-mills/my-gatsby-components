6c6cdb9c25b175aa0a5fbb4c7c9cbe790b27dec0
Kate Mills <kate.mills1124@gmail.com>
2020-09-03 17:23:13 -0700

    Move close btn to line up in center

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -56,7 +56,7 @@ const WrappedAnnouncement = styled(Announcement)`
     white-space: nowrap;
   }
   & .grid-item:last-child {
-    align-items: start;
+    align-items: center;
   }
   & button {
     line-height: 17px;
