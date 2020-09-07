a2af2fe2320f803d56ccb4b4af07c489976f09fe
Kate Mills <kate.mills1124@gmail.com>
2020-09-06 18:26:05 -0700

    Removed console.log

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Video = ({ src, title, ...props }) => {
-      console.log("video", src)
   return (
     <IframeVideoWrapper>
       <div className="video">
